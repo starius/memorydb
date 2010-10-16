cfd9320fe81c858c9ae41350749ef8ea42e1bd2d
boris <bnagaev@gmail.com>
2010-10-17 00:26:09 +0400

    id packing base_link_set impl started

--- base_link.h
+++ base_link.h
@@ -1,21 +1,24 @@
 #ifndef MEMORYDB_BASE_LINK_H_
 #define MEMORYDB_BASE_LINK_H_
 
+#include "id.h"
+
 namespace memorydb {
 
 class BaseLink
 {
 public:
 	void* get() const { return ptr_; }
-	void unload_simple(void* ptr, int ID) { set_simple(int ID); }
+	void unload_simple(void* ptr, int ID) { set_simple(ID); }
 	void load_simple(void* ptr, int ID) { set_simple(ptr); }
 	void delete_simple(void* ptr) { delete_simple(); }
 	void delete_simple(int ID) { delete_simple(); }
+	
+	void set_simple(void* ptr) { ptr_ = ptr; }
+	void set_simple(int ID) { ptr_ = id_pack(ID); }
+	void delete_simple() { ptr_ = NULL; }
 private:
 	void* ptr_;
-	void set_simple(void* ptr) { ptr_ = ptr; }
-	void set_simple(int ID) { ptr_ = (ID<<1) | 1; }
-	void delete_simple() { ptr_ = NULL; }
 };
 
 }
--- base_link_set.h
+++ base_link_set.h
@@ -1,9 +1,21 @@
 #ifndef MEMORYDB_BASELINK_SET_H_
 #define MEMORYDB_BASELINK_SET_H_
 
-#include <vector>
+#include <boost/foreach.hpp>
 
 #include "base_link.h"
+
+Методы BaseLink и BaseLinkSet (указатели везде тут BaseLink или BaseLinkSet) 
+	unload_simple(указатель, ID) -- исправиль указанный указатель внутри себя на соответствующий ID
+	load_simple(указатель, ID) -- обратное к unload_simple
+	delete_simple(указатель или ID):
+		BaseLink: установить соотв. указатель или ID на NULL
+		BaseLinkSet: удалить соотв. указатель или ID на NULL из себя
+			если ordered=false и Container!=list, последний элемент становится на место удаленного
+			если ordered=true, происходит сдвиг, чтобы сохранить порядок следования
+			в случае multi=true, удаляется одна связь, а не все
+	set_simple(указатель или ID) -- для BaseLink устанавливает его смотрящим на это
+		для BaseLinkSet, добавляет элемент к списку
 
 namespace memorydb {
 
@@ -11,9 +23,44 @@ template<bool ordered=false, bool multi=false, typename Container=std::vector>
 class BaseLinkSet
 {
 public:
+	void* get() const { return ptr_; }
+	void unload_simple(void* ptr, int ID);
+	void load_simple(void* ptr, int ID);
+	void delete_simple(void* ptr) { delete_simple(); }
+	void delete_simple(int ID) { delete_simple(); }
 private:
 	Container<BaseLink> refs_;
 };
+
+
+template<>
+void BaseLinkSet::unload_simple(void* ptr, int ID) 
+{
+	BOOST_FOREACH(BaseLink& ref, refs_)
+	{
+		if (ref == ptr)
+		{
+			ref.set_simple(ID);
+			return;
+		}
+	}
+}
+
+template<>
+void BaseLinkSet::load_simple(void* ptr, int ID) 
+{
+	register void* pattern = id_pack(ID);
+	BOOST_FOREACH(BaseLink& ref, refs_)
+	{
+		if (ref == pattern)
+		{
+			ref.set_simple(ptr);
+			return;
+		}
+	}
+}
+
+
 
 }
 
