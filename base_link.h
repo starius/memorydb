591d2c77d72fea379372eb8c7af654ed7df11295
boris <bnagaev@gmail.com>
2010-10-17 00:11:12 +0400

    baselink implementation

--- base_link.h
+++ base_link.h
@@ -6,10 +6,16 @@ namespace memorydb {
 class BaseLink
 {
 public:
-	void set(void* ptr) { ptr_ = ptr; }
 	void* get() const { return ptr_; }
+	void unload_simple(void* ptr, int ID) { set_simple(int ID); }
+	void load_simple(void* ptr, int ID) { set_simple(ptr); }
+	void delete_simple(void* ptr) { delete_simple(); }
+	void delete_simple(int ID) { delete_simple(); }
 private:
 	void* ptr_;
+	void set_simple(void* ptr) { ptr_ = ptr; }
+	void set_simple(int ID) { ptr_ = (ID<<1) | 1; }
+	void delete_simple() { ptr_ = NULL; }
 };
 
 }
